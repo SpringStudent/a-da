4734aa9ae7b04d3c8e71304f5a9709f4f8dbee22
unknown <2267431887@qq.com>
2025-03-19 10:44:52 +0800

    添加将服务注册到注册中心

---
+++ transport/src/main/java/io/github/springstudent/ada/transport/server/NettyController.java
@@ -0,0 +1,26 @@
+package io.github.springstudent.ada.transport.server;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author ZhouNing
+ * @date 2025/3/19 10:44
+ **/
+@RestController
+@RequestMapping("/netty")
+public class NettyController {
+
+    @Value("${netty.server.port}")
+    private Integer serverPort;
+
+    @Value("${netty.server.ip}")
+    private String serverIp;
+
+    @GetMapping("/server")
+    public String server() {
+        return serverIp + ":" + serverPort;
+    }
+}
