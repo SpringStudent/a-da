c4de3db679ca8abb1ac5f13dda896d04b9844442
unknown <2267431887@qq.com>
2025-03-20 09:37:56 +0800

    fix

--- client/src/main/java/io/github/springstudent/ada/client/RemoteClient.java
+++ client/src/main/java/io/github/springstudent/ada/client/RemoteClient.java
@@ -228,7 +228,9 @@ public class RemoteClient extends RemoteFrame {
 
 
     public static void main(String[] args) throws Exception {
+        //不需要注册中心的单机部署
 //        new RemoteClient("172.16.1.37", 11112, "http://172.16.1.37:11111/transport", "http://172.16.1.37:11110/stream");
+        //注册中心部署
         new RemoteClient("http://172.16.1.37:11113");
     }
 
